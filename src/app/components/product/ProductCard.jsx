1e786a55688a703596ccf095268c97c571875dea
vivekdudhatra <vk.dudhatra@gmail.com>
2024-09-18 17:56:32 +0530

    ADDED

--- src/app/components/product/ProductCard.jsx
+++ src/app/components/product/ProductCard.jsx
@@ -1,9 +1,57 @@
+import Image from "next/image";
 import React from "react";
 
-const ProductCard = () => {
+const ProductCard = ({
+  cardImage = "allswell_image",
+  banner_Text = false,
+  price = "$123",
+  last_price = "$123",
+  index,
+}) => {
   return (
     <>
-      <div></div>
+      <div className="w-[394px] border  bg-[#EAE8E2] p-5 relative" key={index}>
+        <Image
+          src={`/images/${cardImage}.png`}
+          className="w-full h-[199px] "
+          alt={cardImage}
+          height={0}
+          width={0}
+          sizes="100vw"
+        />
+        {banner_Text && (
+          <p className="px-2 py-1 text-white bg-[#FF004D] rounded-[2px] w-fit absolute top-[30px] left-8">
+            {banner_Text}
+          </p>
+        )}
+        <h2 className=" font-extrabold font-libre-franklin text-[24px] leading-[29.09px] text-[#171717] mt-5 py-1">
+          Saatva Classic
+        </h2>
+        <p className=" font-poppins font-normal leading-[24px] text-[16px] mt-[10px] pr-5">
+          Includes mattress (One or Dream), Foundation, and Mattress Protector.
+        </p>
+        <p className="text-black font-poppins font-normal leading-[24px] text-[16px] mt-[12px]">
+          <span className="text-black mr-1"> ✔</span> Foundation
+        </p>
+        <p className="text-black font-poppins font-normal leading-[24px] text-[16px] mt-[12px]">
+          <span className="text-black mr-1"> ✔</span> Waterproof Mattress
+          Protector
+        </p>
+        <hr className="w-full h-[2px] bg-[#7B6E62] mt-[15px]" />
+        <div className="flex gap-x-5 items-center mt-[20px]">
+          <h1 className="text-black font-poppins font-bold leading-[60px] text-[40px]  py-1">
+            {price}
+          </h1>
+          <p className="text-[#7B6E62] font-poppins font-medium leading-[36px] text-[24px]  py-1">
+            {last_price}
+          </p>
+        </div>
+        <div className="w-full mt-[6px]">
+          <button className="text-white font-poppins font-medium leading-[27px] text-[18px] h-[55px] bg-[#7B6E62] w-full">
+            Add to Cart
+          </button>
+        </div>
+      </div>
     </>
   );
 };
--- src/app/product/page.jsx
+++ src/app/product/page.jsx
@@ -1,13 +1,93 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import SubHeader from "../components/about/SubHeader";
 import ProductCard from "../components/product/ProductCard";
-
+import ReactPaginate from "react-paginate";
+import { BsArrowLeft } from "react-icons/bs";
+import { BsArrowRight } from "react-icons/bs";
+import "./product.css";
+import CustomerReview from "../components/home/CustomerReview";
+import Help_Deciding from "../components/home/Help_Deciding";
+import Footer from "../components/common/Footer";
 const Product = () => {
+  const itemsPerPage = 9;
+  const items = [
+    { id: 1, banner_Text: "On Sale" },
+    { id: 2, banner_Text: "Special Offer" },
+    { id: 3 },
+    { id: 4 },
+    { id: 5, banner_Text: "On Sale" },
+    { id: 6 },
+    { id: 7 },
+    { id: 8 },
+    { id: 9 },
+    { id: 10 },
+    { id: 11 },
+    { id: 12 },
+    { id: 13 },
+    { id: 14 },
+    { id: 15 },
+    { id: 16 },
+    { id: 17 },
+    { id: 18 },
+    { id: 19 },
+    { id: 20 },
+  ]; // Add more items as needed
+
+  const [currentPage, setCurrentPage] = useState(0);
+
+  const handlePageClick = (data) => {
+    setCurrentPage(data.selected);
+  };
+
+  const offset = currentPage * itemsPerPage;
+  const currentItems = items.slice(offset, offset + itemsPerPage);
+
   return (
-    <div className=" box-border p-0 m-0">
+    <div className="box-border p-0 m-0">
       <SubHeader title="Mattresses" />
-      <div>
-        <ProductCard />
+      <div className="mt-[100px] container mx-auto">
+        <div className="flex flex-wrap justify-center items-center gap-[30px]">
+          {currentItems.map((item, index) => (
+            <ProductCard
+              key={item.id}
+              banner_Text={item.banner_Text}
+              index={index}
+            />
+          ))}
+        </div>
+      </div>
+
+      {/* Pagination */}
+      <div className="mt-[70px]">
+        <ReactPaginate
+          previousLabel={
+            <BsArrowLeft className="w-[50px] h-[35px] text-[#000000]" />
+          }
+          nextLabel={
+            <BsArrowRight className="w-[50px] h-[35px] text-[#000000]" />
+          }
+          breakLabel={"..."}
+          pageCount={Math.ceil(items.length / itemsPerPage)}
+          marginPagesDisplayed={2}
+          pageRangeDisplayed={5}
+          onPageChange={handlePageClick}
+          containerClassName={"pagination"}
+          activeClassName={"active"}
+          previousLinkClassName={"previous"}
+          nextLinkClassName={"next"}
+          pageLinkClassName={"page"}
+          breakLinkClassName={"break"}
+        />
+      </div>
+      <div className="mt-[100px]">
+        <CustomerReview />
+      </div>
+      <div className="mt-[189px]">
+        <Help_Deciding />
+      </div>
+      <div className="mt-[100px]">
+        <Footer />
       </div>
     </div>
   );
